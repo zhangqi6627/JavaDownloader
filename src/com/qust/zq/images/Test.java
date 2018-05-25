fe5e4854c705998bda6eed4838a2a1ad87b84491
zhangqi <zhangqi@sagereal.com>
2018-05-25 14:01:23 +0800

    hello world

--- src/com/qust/zq/images/Test.java
+++ src/com/qust/zq/images/Test.java
@@ -5,9 +5,9 @@ public class Test {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		String imageUrl = "http://www.baidu.com/a.jpg";
 		String subString = imageUrl.substring(imageUrl.lastIndexOf("."));
 		System.out.println(subString);
+		System.out.println("hello world");
 	}
 }
