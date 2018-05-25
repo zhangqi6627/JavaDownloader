3fd4764ef72be9cf814b088f03295bc6682efe83
zhangqi <zhangqi@sagereal.com>
2018-05-25 17:00:58 +0800

    update

---
+++ src/com/qust/zq/images/Constants.java
@@ -0,0 +1,8 @@
+package com.qust.zq.images;
+
+public class Constants {
+	public final static String DOWNLOAD_PATH = "/home/zq/MyFavorites/";
+	public final static String DOWNLOAD_IMAGE_PATH = "";
+	public final static String DOWNLOAD_VIDEO_PATH = "";
+	public final static String DOWNLOAD_NOVAL_PATH = "";
+}
