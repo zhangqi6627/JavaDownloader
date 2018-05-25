c8f63a7ae6e4755303bf4ca99dc64a98409d58c0
zhangqi <zhangqi@sagereal.com>
2018-05-25 14:18:24 +0800

    update project

--- src/com/qust/zq/images/Test.java
+++ src/com/qust/zq/images/Test.java
@@ -1,14 +1,6 @@
 package com.qust.zq.images;
 
 public class Test {
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
-		String imageUrl = "http://www.baidu.com/a.jpg";
-		String subString = imageUrl.substring(imageUrl.lastIndexOf("."));
-		System.out.println(subString);
-		Sysou
-		System.out.println("hello world");
 	}
 }
