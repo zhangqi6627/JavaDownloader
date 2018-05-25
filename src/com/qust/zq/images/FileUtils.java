c6c26716dfe25fc6a1485c2d66bb658fd61daafe
zhangqi <zhangqi@sagereal.com>
2018-05-25 17:00:35 +0800

    update

--- src/com/qust/zq/images/FileUtils.java
+++ src/com/qust/zq/images/FileUtils.java
@@ -18,18 +18,16 @@ public class FileUtils {
 		out.close();
 	}
 
-    public static boolean deleteDir(File dir) {
-        if (dir.isDirectory()) {
-            String[] children = dir.list();
-            //递归删除目录中的子目录下
-            for (int i=0; i<children.length; i++) {
-                boolean success = deleteDir(new File(dir, children[i]));
-                if (!success) {
-                    return false;
-                }
-            }
-        }
-        // 目录此时为空，可以删除
-        return dir.delete();
-    }
+	public static boolean deleteDir(File dir) {
+		if (dir.isDirectory()) {
+			String[] children = dir.list();
+			for (int i = 0; i < children.length; i++) {
+				boolean success = deleteDir(new File(dir, children[i]));
+				if (!success) {
+					return false;
+				}
+			}
+		}
+		return dir.delete();
+	}
 }
--- src/com/qust/zq/images/Test.java
+++ src/com/qust/zq/images/Test.java
@@ -1,6 +1,9 @@
 package com.qust.zq.images;
 
+import java.io.File;
+
 public class Test {
 	public static void main(String[] args) {
+		File file = new File("");
 	}
 }
